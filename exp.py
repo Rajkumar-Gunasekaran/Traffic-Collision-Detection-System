61acec6290c2727dc1e127695232b060d4163977
Rajkumar Gunasekaran <gmkrajkumar@gmail.com>
2023-10-11 09:29:36 +0530

    update
    able to detect cars and track it with local and live video source

--- exp.py
+++ exp.py
@@ -1,49 +1,36 @@
 import cv2
+import numpy as np
 
-def calculate_centroid(contour):
-    M = cv2.moments(contour)
-    area = M["m00"]
-    if area > 0:
-        cx = int(M["m10"] / area)
-        cy = int(M["m01"] / area)
-        return cx, cy
-    else:
-        return None, None
+# Load the pre-trained car detection model
+car_cascade = cv2.CascadeClassifier('haarcascade_car.xml')  # You can replace this with a more accurate model
 
-video_capture = cv2.VideoCapture(0)
-background_subtractor = cv2.createBackgroundSubtractorMOG2()
-tracked_objects = {}
-object_id_counter = 0 
+# Create a video capture object for the default camera (change the index if necessary)
+video_source = "car_-_2165 (Original).mp4"
+video_capture = cv2.VideoCapture(video_source)
+
+# Define a function to detect and track cars in each frame
+def track_cars(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cars = car_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in cars:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    return frame
 
 while True:
     ret, frame = video_capture.read()
-
     if not ret:
         break
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    fg_mask = background_subtractor.apply(gray_frame)
 
-    _, binary_mask = cv2.threshold(fg_mask, 200, 255, cv2.THRESH_BINARY)
+    # Call the track_cars function to detect and track cars in the frame
+    output_frame = track_cars(frame)
 
-    contours, _ = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for contour in contours:
-        x, y, w, h = cv2.boundingRect(contour)
-        cx, cy = calculate_centroid(contour)
+    cv2.imshow('Car Tracking', output_frame)
 
-        # Calculate aspect ratio
-        aspect_ratio = float(w) / h
-
-        # Filter based on aspect ratio and area (adjust these thresholds as needed)
-        if aspect_ratio >= 0.5 and aspect_ratio <= 2.0 and cv2.contourArea(contour) > 1000:
-            object_id_counter += 1
-            object_id = object_id_counter 
-            tracked_objects[object_id] = (cx, cy)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.circle(frame, (cx, cy), 4, (0, 255, 0), -1)
-
-    cv2.imshow('Vehicle and Rectangular Object Tracking', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' key to exit
         break
-    
+
+# Release the video capture and close all OpenCV windows
 video_capture.release()
 cv2.destroyAllWindows()
